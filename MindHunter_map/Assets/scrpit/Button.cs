79b7c3d3a6362fca9cc425ad721eac31b6646282
chenyilin0810 <chenyilin0810@qq.com>
2018-10-28 19:02:07 +0800

    修改button

--- MindHunter_map/Assets/scrpit/Button.cs
+++ MindHunter_map/Assets/scrpit/Button.cs
@@ -8,8 +8,8 @@ public class Button : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+     }
 
 
     // Update is called once per frame
